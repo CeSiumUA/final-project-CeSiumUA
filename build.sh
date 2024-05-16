76d4c55654261ecbbb1fe8fd7e4786d9a58ac160
CeSiumUA <fedir.katushonok@gmail.com>
2024-05-16 08:54:15 +0300

    Fixed build script

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@
 
 source shared.sh
 
-EXTERNAL_REL_BUILDROOT=../base_external
+EXTERNAL_REL_BUILDROOT_FOLDER_NAME=base_external
+EXTERNAL_REL_BUILDROOT=../$EXTERNAL_REL_BUILDROOT_FOLDER_NAME
 git submodule init
 git submodule sync
 git submodule update
@@ -13,7 +14,7 @@ set -e
 cd `dirname $0`
 
 # Check if wpa_supplicant.conf exists
-if [ ! -e $EXTERNAL_REL_BUILDROOT/rootfs_overlay/etc/wpa_supplicant.conf ]
+if [ ! -e $EXTERNAL_REL_BUILDROOT_FOLDER_NAME/rootfs_overlay/etc/wpa_supplicant.conf ]
 then
     echo "MISSING WPA_SUPPLICANT CONFIGURATION FILE"
     ./generate_wpa_conf.sh
