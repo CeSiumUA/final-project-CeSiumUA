6ddc56556fbd4910a46f2543fb1bd63093799b68
CeSiumUA <fedir.katushonok@gmail.com>
2024-05-28 19:41:31 +0300

    chore: Add htop package to the build configuration

--- base_external/package/cesium-test-pkg/cesium-test-pkg.mk
+++ base_external/package/cesium-test-pkg/cesium-test-pkg.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-CESIUM_TEST_PKG_VERSION = '6853c841cc612805ef03cc9884a3dee913a58d2d'
+CESIUM_TEST_PKG_VERSION = '491c4dacc88402bba3d7b60fbcf06519b275cdde'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
