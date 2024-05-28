dba71d4e8688687521b57a9cb21b176e8eeadb40
CeSiumUA <fedir.katushonok@gmail.com>
2024-05-28 09:19:55 +0300

    Added new package

--- base_external/package/cesium_test_pkg/cesium_test_pkg.mk
+++ base_external/package/cesium_test_pkg/cesium_test_pkg.mk
@@ -1,7 +1,8 @@
 CESIUM_TEST_PKG_VERSION = "1.0.0"
 
-CESIUM_TEST_PKG_SITE = file://./
-CESIUM_TEST_PKG_SITE_METHOD = file
+CESIUM_TEST_PKG_SITE := ./cesium_test_pkg
+CESIUM_TEST_PKG_SITE_METHOD := local
+CESIUM_TEST_PKG_INSTALL_TARGET := YES
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
