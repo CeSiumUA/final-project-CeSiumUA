30b749509bff4f71285da7d2d4d4f7b184a79991
CeSium <fedir.katushonok@gmail.com>
2024-06-14 03:59:35 +0000

    Added test overlay package

---
+++ base_external/package/test-overlay-pkg/test-overlay-pkg.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# cesium-test-pkg
+#
+##############################################################
+
+TEST_OVERLAY_PKG_VERSION = 'c93e0c6a0aa072cacb64c65813918bcf646477c5'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+TEST_OVERLAY_PKG_SITE = 'git@github.com:CeSiumUA/lkm-test.git'
+TEST_OVERLAY_PKG_SITE_METHOD = git
+TEST_OVERLAY_PKG_GIT_SUBMODULES = YES
+
+define TEST_OVERLAY_PKG_INSTALL_CMDS
+
+mkdir -p $(TARGET_DIR)/boot/overlays
+dtc -@ -I dts -O dtb -o $(@D)/testoverlay.dtbo $(@D)/testoverlay.dts
+cp -f $(@D)/testoverlay.dtbo $(TARGET_DIR)/boot/overlays
+awk -v insert="  FDTOVERLAYS /boot/overlays/testoverlay.dtbo" '/devicetreedir \/boot/ { print; print insert; next }1' $(TARGET_DIR)/boot/extlinux/extlinux.conf > /tmp/extlinux.conf
+mv /tmp/extlinux.conf $(TARGET_DIR)/boot/extlinux/extlinux.conf
+
+endef
+
+$(eval $(generic-package))
