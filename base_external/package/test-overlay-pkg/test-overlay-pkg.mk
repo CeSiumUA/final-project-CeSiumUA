9ba5004934b9e0f93a7637c840b938c4b693d032
CeSium <fedir.katushonok@gmail.com>
2024-06-15 05:25:15 +0000

    Fixed wrong overlay file

--- base_external/package/test-overlay-pkg/test-overlay-pkg.mk
+++ base_external/package/test-overlay-pkg/test-overlay-pkg.mk
@@ -14,17 +14,13 @@ TEST_OVERLAY_PKG_SITE_METHOD = git
 TEST_OVERLAY_PKG_GIT_SUBMODULES = YES
 
 define TEST_OVERLAY_PKG_BUILD_CMDS
-	echo "Building testoverlay.dtbo"
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) dt
-	echo "Building testoverlay.dtbo done"
 endef
 
 define TEST_OVERLAY_PKG_INSTALL_TARGET_CMDS
-	echo "Installing testoverlay.dtbo"
 	mkdir -p $(TARGET_DIR)/boot/overlays
 	cp -f $(@D)/testoverlay.dtbo $(TARGET_DIR)/boot/overlays
 	file $(TARGET_DIR)/boot/overlays/testoverlay.dtbo
-	echo "Installing testoverlay.dtbo done"
 endef
 
 $(eval $(generic-package))
