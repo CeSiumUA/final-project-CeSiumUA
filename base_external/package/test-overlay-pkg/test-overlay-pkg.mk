3679a19c89a0d1fe54149efac22b232e2490bf43
CeSium <fedir.katushonok@gmail.com>
2024-06-15 04:49:25 +0000

    Update CESIUM_TEST_PKG_VERSION and TEST_OVERLAY_PKG_VERSION

--- base_external/package/test-overlay-pkg/test-overlay-pkg.mk
+++ base_external/package/test-overlay-pkg/test-overlay-pkg.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-TEST_OVERLAY_PKG_VERSION = 'c93e0c6a0aa072cacb64c65813918bcf646477c5'
+TEST_OVERLAY_PKG_VERSION = 'da0a17fc18f6457b55e1a06e6aaf9c3e417e2291'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -13,12 +13,13 @@ TEST_OVERLAY_PKG_SITE = 'git@github.com:CeSiumUA/lkm-test.git'
 TEST_OVERLAY_PKG_SITE_METHOD = git
 TEST_OVERLAY_PKG_GIT_SUBMODULES = YES
 
+define TEST_OVERLAY_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) dts
+endef
+
 define TEST_OVERLAY_PKG_INSTALL_CMDS
-
-mkdir -p $(TARGET_DIR)/boot/overlays
-dtc -@ -I dts -O dtb -o $(@D)/testoverlay.dtbo $(@D)/testoverlay.dts
-cp -f $(@D)/testoverlay.dtbo $(TARGET_DIR)/boot/overlays
-
+	mkdir -p $(TARGET_DIR)/boot/overlays
+	cp -f $(@D)/testoverlay.dtbo $(TARGET_DIR)/boot/overlays
 endef
 
 $(eval $(generic-package))
