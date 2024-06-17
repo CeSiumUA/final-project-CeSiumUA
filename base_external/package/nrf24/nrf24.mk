92557f9fceefa9cc063ca7ecb3d64ebf17cdbc9a
CeSium <fedir.katushonok@gmail.com>
2024-06-17 14:40:53 +0000

    moved to a single nrf package

--- base_external/package/nrf24/nrf24.mk
+++ base_external/package/nrf24/nrf24.mk
@@ -1,26 +1,27 @@
 
 ##############################################################
 #
-# cesium-test-pkg
+# nrf24
 #
 ##############################################################
 
-TEST_OVERLAY_PKG_VERSION = '71b83a29acdefbdea94a8d505f43f0ed43d1e801'
+NRF24_VERSION = 'a470a857a4e17392815f535207c210f432f7e21c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-TEST_OVERLAY_PKG_SITE = 'git@github.com:CeSiumUA/lkm-test.git'
-TEST_OVERLAY_PKG_SITE_METHOD = git
-TEST_OVERLAY_PKG_GIT_SUBMODULES = YES
+NRF24_SITE = 'git@github.com:CeSiumUA/lkm-test.git'
+NRF24_SITE_METHOD = git
+NRF24_GIT_SUBMODULES = YES
 
-define TEST_OVERLAY_PKG_BUILD_CMDS
+define NRF24_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) dt
 endef
 
-define TEST_OVERLAY_PKG_INSTALL_TARGET_CMDS
+define NRF24_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/boot/overlays
 	cp -f $(@D)/testoverlay.dtbo $(TARGET_DIR)/boot/overlays
 	file $(TARGET_DIR)/boot/overlays/testoverlay.dtbo
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
