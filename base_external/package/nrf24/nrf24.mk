02340e05f12cfe87d0513f88ea5ac025232df879
CeSiumUA <fedir.katushonok@gmail.com>
2024-06-21 07:19:11 +0300

    tmp

--- base_external/package/nrf24/nrf24.mk
+++ base_external/package/nrf24/nrf24.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-NRF24_VERSION = 'a470a857a4e17392815f535207c210f432f7e21c'
+NRF24_VERSION = '5ded6e49dc6362a32e8f732b43b47d4dcf7b441e'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
