6842dcb7f1f7466cc8c4fb307fdc95b438ea2f11
Fedir Katushonok <46378315+CeSiumUA@users.noreply.github.com>
2024-06-27 20:13:20 +0300

    9 add auto build on new commits to master using GitHub actions (#10)
    * feat: Add support for user-defined WiFi SSID and password
    
    This commit modifies the `generate_wpa_conf.sh` script to allow the user to provide the WiFi SSID and password as environment variables (`BR_PROJECT_WIFI_SSID` and `BR_PROJECT_WIFI_PASSWORD`). If these variables are not set, the script prompts the user for the SSID and password. This change improves the flexibility and usability of the script.
    
    * Added basic image build on push to master and this branch
    
    * Added execution permission to clean-build.sh
    
    * Fixed typo
    
    * Added SSID and Password from secrets
    
    * Added build on pull-request

--- generate_wpa_conf.sh
+++ generate_wpa_conf.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# Prompt the user for the SSID and password
-read -p "Enter the SSID: " ssid
-read -sp "Enter the password: " password
+if [ -z "${BR_PROJECT_WIFI_SSID}" ]; then
+    # Prompt the user for the SSID
+    read -p "Enter the SSID: " ssid
+else
+    ssid="${BR_PROJECT_WIFI_SSID}"
+fi
+
+if [ -z "${BR_PROJECT_WIFI_PASSWORD}" ]; then
+    # Prompt the user for the password
+    read -sp "Enter the password: " password
+else
+    password="${BR_PROJECT_WIFI_PASSWORD}"
+fi
 
 # Generate the wpa_supplicant.conf file
 cat << EOF > base_external/rootfs_overlay/etc/wpa_supplicant.conf
