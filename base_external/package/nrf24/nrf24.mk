3b293016b1aa8dc8f856928ff54499d1c7cf15bc
Fedir Katushonok <46378315+CeSiumUA@users.noreply.github.com>
2024-07-06 22:54:38 +0300

    Added nrf24 Linux driver to Buildroot-based project (#11)
    * Added post build script (to load DTBO)
    
    * Fixed overlay name in postbuild script
    
    * Added nrf24 package
    
    * Updated commit hash

---
+++ base_external/package/nrf24/nrf24.mk
@@ -0,0 +1,27 @@
+
+##############################################################
+#
+# nrf24
+#
+##############################################################
+
+NRF24_VERSION = 'bf491d97aae9b83af36adab73c44d1b568fba121'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+NRF24_SITE = 'git@github.com:CeSiumUA/nrf24l01_linux_driver.git'
+NRF24_SITE_METHOD = git
+NRF24_GIT_SUBMODULES = YES
+
+define NRF24_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) dtbo
+endef
+
+define NRF24_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/boot/overlays
+	cp -f $(@D)/nrf24_overlay.dtbo $(TARGET_DIR)/boot/overlays
+	file $(TARGET_DIR)/boot/overlays/nrf24_overlay.dtbo
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
