2eb56e06fb9658a62b656ea3d340c4e1a4bed2c6
CeSium <fedir.katushonok@gmail.com>
2024-07-11 12:53:09 +0300

    Added final testing as target branch

--- base_external/package/nrf24/nrf24.mk
+++ base_external/package/nrf24/nrf24.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-NRF24_VERSION = '7f73a52bda63687bf283420e63766fac5a830607'
+NRF24_VERSION = 'final-testing'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
