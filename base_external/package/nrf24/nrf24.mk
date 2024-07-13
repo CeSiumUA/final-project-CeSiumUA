eeb81c9a96c6351887525e716dfd9f204e5fb592
CeSium <fedir.katushonok@gmail.com>
2024-07-13 20:20:31 +0300

    Added libpgio packages and updated nrf24 commit hash

--- base_external/package/nrf24/nrf24.mk
+++ base_external/package/nrf24/nrf24.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-NRF24_VERSION = 'bf491d97aae9b83af36adab73c44d1b568fba121'
+NRF24_VERSION = 'b8617d7b315797dca567f235e00a69a3d8477997'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
