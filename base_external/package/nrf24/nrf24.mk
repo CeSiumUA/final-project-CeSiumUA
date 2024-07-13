cd34f677c12369694e46ab82bcd8818a9e00d8e1
Fedir Katushonok <46378315+CeSiumUA@users.noreply.github.com>
2024-07-14 00:00:50 +0300

    Updated project after testing of nrf24 drivers (#13)
    * Added libpgio packages and updated nrf24 commit hash
    
    * Updated nrf24 package hash

--- base_external/package/nrf24/nrf24.mk
+++ base_external/package/nrf24/nrf24.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-NRF24_VERSION = 'bf491d97aae9b83af36adab73c44d1b568fba121'
+NRF24_VERSION = '678eee66651d34c869da0bbadd2c566f81fb5e6b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
