412a64ae727181e9938217af1f2960d795b72c84
CeSium <fedir.katushonok@gmail.com>
2024-07-13 21:46:58 +0300

    Updated nrf24 package hash

--- base_external/package/nrf24/nrf24.mk
+++ base_external/package/nrf24/nrf24.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-NRF24_VERSION = 'b8617d7b315797dca567f235e00a69a3d8477997'
+NRF24_VERSION = '678eee66651d34c869da0bbadd2c566f81fb5e6b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
